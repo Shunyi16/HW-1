c915dfc7e8bb9de4981584347b6d0e7fe35ab20c
Shunyi Liu <shunyi.liu16@gmail.com>
2021-09-12 18:04:24 -0700

    changed the second class's name and recoded it

--- src/MyDog.java
+++ src/MyDog.java
@@ -1,8 +1,8 @@
-public class Dog {
+public class MyDog {
     public static void main(String[] args) {
-        HelloCS151 name = new HelloCS151();
-        HelloCS151 age = new HelloCS151();
-        System.out.println("My dog's name is " + name.myDog);
+        DogInformation dog = new DogInformation(null, 0);
+        DogInformation age = new DogInformation(null, 0);
+        System.out.println("My dog's name is " + dog.name);
         System.out.println("He is " + age.age + " years old");
     }
 }
