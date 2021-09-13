3fc73eebb0f94d6aac862ff1be9088dd9ce46c56
Shunyi Liu <shunyi.liu16@gmail.com>
2021-09-12 17:19:11 -0700

    added my dog's information

---
+++ src/Dog.java
@@ -0,0 +1,8 @@
+public class Dog {
+    public static void main(String[] args) {
+        HelloCS151 name = new HelloCS151();
+        HelloCS151 age = new HelloCS151();
+        System.out.println("My dog's name is " + name.myDog);
+        System.out.println("He is " + age.age + " years old");
+    }
+}
