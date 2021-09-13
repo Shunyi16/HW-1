5cea176b43656d7b507e695a8b7b1d9faf8ff403
Shunyi Liu <shunyi.liu16@gmail.com>
2021-09-12 17:59:42 -0700

    Changed first class's name and made the code better

---
+++ src/DogInformation.java
@@ -0,0 +1,16 @@
+public class HelloCS151 {
+    String name;
+    int age;
+
+    /**
+     *
+     * @param name
+     * @param age
+     */
+    public HelloCS151(String name, int age){
+        this.name = "Joe";
+        this.age = 15;
+    }
+}
+
+
