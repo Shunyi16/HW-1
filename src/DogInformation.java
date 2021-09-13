7fc32061ee6ff8c2af5c72e7ccd6bc4fe89b50b0
Shunyi Liu <shunyi.liu16@gmail.com>
2021-09-12 18:22:28 -0700

    added javadoc comments for the class, constructor, and parameters

--- src/DogInformation.java
+++ src/DogInformation.java
@@ -1,7 +1,15 @@
+/**
+ * class DogInformation for MyDog's simple information
+ */
 public class DogInformation {
     String name;
     int age;
 
+    /**
+     *the instructor is to initialize the values of dog's name and age
+     * @param name is the name of the dog
+     * @param age is the age of the dog
+     */
     public DogInformation(String name, int age){
         this.name = "Joe";
         this.age = 15;
